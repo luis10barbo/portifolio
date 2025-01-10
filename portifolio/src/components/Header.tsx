2cf85400c9452a7a90bfa4adb4c43590de0430b6
luis10barbo <luis10barbo2020@gmail.com>
2025-01-10 16:49:37 -0300

    Trocar entre ingles e portugues

--- portifolio/src/components/Header.tsx
+++ portifolio/src/components/Header.tsx
@@ -3,6 +3,7 @@ import Button from "./Button";
 import Brazil from "@/../public/bandeiras/brazil.svg"
 import Uk from "@/../public/bandeiras/uk.svg"
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({locale}));
@@ -13,10 +14,14 @@ export default function Header({locale}: {locale: string}) {
     
     return <header className="p-2 lg:p-8 flex flex-row-reverse fixed top-0 w-full bg-white z-30">
         {locale === "pt" ? 
-        <Button id="language-button" className="text-base px-4 py-2 bg-green-200 border border-neutral-200 " icone={Brazil}>{t("portugues")}</Button>
+        <Link href={"/en"}>
+            <Button id="language-button" className="text-base px-4 py-2 bg-green-200 border border-neutral-200 " icone={Brazil}>{t("portugues")}</Button>
+        
+        </Link>
     :
+    <Link href={"/pt"}>
         <Button id="language-button" className="text-base px-4 py-2 bg-red-200 border border-neutral-200 " icone={Uk}>{t("ingles")}</Button>
-
+    </Link>
     }
     </header>
 }

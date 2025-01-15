b4c84a8bf8d5fa753c84506b78a833d8de9a60a8
luis10barbo <luis10barbo2020@gmail.com>
2025-01-15 08:35:34 -0300

    Arrumadas imagens em projeto

--- portifolio/src/app/[locale]/proj/[id]/page.tsx
+++ portifolio/src/app/[locale]/proj/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { projetos } from "@/data/data"
-// import Image from "next/image";
+import Image from "next/image";
 
 export default async function ProjetoPage({params}: {params: Promise<{id: string}>}) {
     const paramsAwaited = await params;
@@ -11,9 +11,14 @@ export default async function ProjetoPage({params}: {params: Promise<{id: string
     if (!projeto) {
         return <></>;
     }
-    return <div className="mt-28 p-4 lg:w-[1280px]">
-        <h2 className="text-7xl">{projeto.titulo}</h2>
-        PAGINA EM CONSTRUCAO
+    return <div className="mt-28 py-16 lg:w-[1280px] m-auto">
+        <h2 className="text-9xl font-bold">{projeto.titulo}</h2>
+        <p className="text-5xl text-neutral-700 pt-4">{projeto.descricao}</p>
+        <div id="imagens" className="flex gap-4 pt-12">
+            {projeto.imgProjeto.map((img, i) => {
+                return <Image key={`img ${i}`} src={img} width={1920} height={1080} alt={`imagem projeto ${i + 1}`} className="shadow-lg rounded-lg max-h-[1080px] object-contain"></Image>
+            })}
+        </div>
         {/* <Image src={projeto.imgProjeto[0]} width={1920} height={1080}  alt="imagem projeto"/> */}
     </div>
 }

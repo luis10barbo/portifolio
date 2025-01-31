866cc15bcd0cedca1db88f268461abe7a4898529
luis10barbo <luis10barbo2020@gmail.com>
2025-01-31 09:10:00 -0300

    Responsividade de textos em pagina projeto

--- portifolio/src/app/[locale]/proj/[id]/page.tsx
+++ portifolio/src/app/[locale]/proj/[id]/page.tsx
@@ -11,11 +11,11 @@ export default async function ProjetoPage({params}: {params: Promise<{id: string
         return <></>;
     }
     return <div className="mt-28 py-16 lg:w-[1280px] m-auto overflow-hidden">
-        <h2 className="text-9xl font-bold">{projeto.titulo}</h2>
-        <p className="text-5xl text-neutral-700 pt-4">{projeto.descricao}</p>
+        <h2 className="sm:text-9xl text-5xl font-bold text-center sm:text-left">{projeto.titulo}</h2>
+        <p className="text-xl sm:text-5xl text-neutral-700 pt-4 sm:text-left text-center">{projeto.descricao}</p>
         <div id="imagens" className="flex gap-4 pt-12 flex-wrap px-4">
             {projeto.imgProjeto.map((img, i) => {
-                return <Image key={`img ${i}`} src={img} width={1920} height={1080} alt={`imagem projeto ${i + 1}`} className="shadow-lg rounded-lg max-h-[720px] object-contain w-fit"></Image>
+                return <Image key={`img ${i}`} src={img} width={1920} height={1080} alt={`imagem projeto ${i + 1}`} className="shadow-lg rounded-lg max-h-[720px] object-contain w-fit flex-1"></Image>
             })}
         </div>
         {/* <Image src={projeto.imgProjeto[0]} width={1920} height={1080}  alt="imagem projeto"/> */}
